d02a56b3176fb5eae6b0e8d10ac100e79048808d
27bslash <greenpear68@yahoo.co.uk>
2023-12-28 16:51:09 +0000

    added download links to star script

--- star-filterer/main.js
+++ star-filterer/main.js
@@ -3,9 +3,11 @@
 // @namespace    http://tampermonkey.net/
 // @version      0.1
 // @description  try to take over the world!
-// @author       You
+// @author       27bslash
 // @match        https://osrsportal.com/shooting-stars-tracker
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=osrsportal.com
+// @updateURL    https://github.com/27bslash/tamperMonkey-scripts/raw/main/star-filterer/main.js
+// @downloadURL  https://github.com/27bslash/tamperMonkey-scripts/raw/main/star-filterer/main.js
 // @grant        none
 // ==/UserScript==
 
